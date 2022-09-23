5214a3850677f834c30dce676dc3de8e1b4bc99c
Pritam <pritamsharma3648@gmail.com>
2022-09-23 12:27:53 +0530

    added snacbar

--- lib/electrificaition.dart
+++ lib/electrificaition.dart
@@ -156,6 +156,17 @@ class _ElectrificationState extends State<Electrification> {
                       child: InkWell(
                         onTap: () {
                           if (_formKey.currentState!.validate()) {}
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text(
+                                "Submitted",
+                                style: TextStyle(),
+                                textAlign: TextAlign.center,
+                              ),
+                              duration: Duration(seconds: 2),
+                              backgroundColor: Colors.indigo,
+                            ),
+                          );
                         },
                         child: Container(
                           width: double.infinity,
